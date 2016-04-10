8684d79dc3e4ac8d133d74fad59f1815f919dd0f
qcamiloq <qcamiloq@gmail.com>
2016-04-10 17:31:03 -0500

    Se crean dos botones colapsables, uno para cargar tornillos y otro para manipular el movimiento de los tornillos

--- moduloTarea1.py
+++ moduloTarea1.py
@@ -29,10 +29,22 @@ class moduloTarea1Widget:
       self.parent.show()
 
   def setup(self):
+
+    sampleCollapsibleButton = ctk.ctkCollapsibleButton() #Se crea boton colapsable
+    sampleCollapsibleButton.text = "Creacion de tornillos" #Se asigna label del boton colapsable
+    sampleCollapsibleButton.collapsed = True #Aparecen sin colapsar
+    self.layout.addWidget(sampleCollapsibleButton) #Se crea layout dentro del boton colapsable
+
+    sample1CollapsibleButton = ctk.ctkCollapsibleButton()  #Se crea boton colapsable
+    sample1CollapsibleButton.text = "Movimiento de tornillo" #Se asigna label del boton colapsable
+    sample1CollapsibleButton.collapsed = True #Aparecen sin colapsar
+    self.layout.addWidget(sample1CollapsibleButton) #Se crea layout dentro del boton colapsable
+
+
     path='C:\Users\Camilo_Q\Documents\MEGA\Trabajo_de_grado\moduloTarea1/Tornillo_1.STL' #Se obtiene direccion de la unbicación del tornillo
     slicer.util.loadModel(path) #Se carga el tornillo al espacio 3d
-    tornillo1=slicer.util.getNode('Tornillo_1')
-    transformada=slicer.vtkMRMLLinearTransformNode()
-    transformada.SetName('Transformada')
-    slicer.mrmlScene.AddNode(transformada)
-    tornillo1.SetAndObserveTransformNodeID(transformada.GetID())
+    tornillo1=slicer.util.getNode('Tornillo_1') #Se obtiene la informacion del tonillo cargado
+    transformada=slicer.vtkMRMLLinearTransformNode() #Se crea una transformada lineal
+    transformada.SetName('Transformada') #Se asigna nombre a la transformada
+    slicer.mrmlScene.AddNode(transformada) #
+    tornillo1.SetAndObserveTransformNodeID(transformada.GetID()) # Se relaciona la trnasformada con el objeto tornillo
