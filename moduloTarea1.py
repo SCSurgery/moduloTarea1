e09aa30f22d7c4508d812488f678474d94db42fb
qcamiloq <qcamiloq@gmail.com>
2016-04-10 17:39:18 -0500

    Se agrega dentro del primer boton colapsable el click boton cargar tornillo, imprime en consola: cargaste un tornillo al ser presionado.

--- moduloTarea1.py
+++ moduloTarea1.py
@@ -30,16 +30,31 @@ class moduloTarea1Widget:
 
   def setup(self):
 
+    #Path para camilo= path='C:\Users\Camilo_Q\Documents\MEGA\Trabajo_de_grado\moduloTarea1/Tornillo_1.STL'
+    #Path para Nicolas = ...
+
     sampleCollapsibleButton = ctk.ctkCollapsibleButton() #Se crea boton colapsable
     sampleCollapsibleButton.text = "Creacion de tornillos" #Se asigna label del boton colapsable
     sampleCollapsibleButton.collapsed = True #Aparecen sin colapsar
     self.layout.addWidget(sampleCollapsibleButton) #Se crea layout dentro del boton colapsable
+
+#-------------------------------------------------------------------------------------------------
 
     sample1CollapsibleButton = ctk.ctkCollapsibleButton()  #Se crea boton colapsable
     sample1CollapsibleButton.text = "Movimiento de tornillo" #Se asigna label del boton colapsable
     sample1CollapsibleButton.collapsed = True #Aparecen sin colapsar
     self.layout.addWidget(sample1CollapsibleButton) #Se crea layout dentro del boton colapsable
 
+#------------------------------------------------------------------------------------------------
+    #Click boton "Cargar tornillo"
+    sampleFormLayout = qt.QFormLayout(sampleCollapsibleButton)
+
+    layout1Button = qt.QPushButton("Cargar tornillo") #Se crea boton pulsable, con texto "Apply"
+    layout1Button.toolTip = "Al presionar aparecerá un tornillo en la scena" #Información que aparece si dejas el mouse encima
+    sampleFormLayout.addWidget(layout1Button) #Se añade el boton al layout del boton colapsable
+    layout1Button.connect('clicked(bool)',self.onApply)
+
+#-----------------------------------------------------------------------------------------------
 
     path='C:\Users\Camilo_Q\Documents\MEGA\Trabajo_de_grado\moduloTarea1/Tornillo_1.STL' #Se obtiene direccion de la unbicación del tornillo
     slicer.util.loadModel(path) #Se carga el tornillo al espacio 3d
@@ -48,3 +63,6 @@ class moduloTarea1Widget:
     transformada.SetName('Transformada') #Se asigna nombre a la transformada
     slicer.mrmlScene.AddNode(transformada) #
     tornillo1.SetAndObserveTransformNodeID(transformada.GetID()) # Se relaciona la trnasformada con el objeto tornillo
+
+  def onApply(self): 
+    print "Cargaste un tornillo"
