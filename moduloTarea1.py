cc16bc8d0d27b3b76f607a7d20a942457c262b01
qcamiloq <qcamiloq@gmail.com>
2016-04-10 18:43:31 -0500

    Se agregan dos botones para cargar dos tornillos, el segundo tornillo se crea matriz de transformada y se translada 5 para que no aparezca superpuesto

--- moduloTarea1.py
+++ moduloTarea1.py
@@ -1,6 +1,7 @@
 # -*- coding: UTF-8 -*-
 from __main__ import vtk, qt, ctk, slicer
 
+
 class moduloTarea1():
   def __init__(self, parent):
     parent.title = "Tarea 1"
@@ -17,6 +18,7 @@ class moduloTarea1():
 
 class moduloTarea1Widget:
   def __init__(self, parent = None):
+
     if not parent:
       self.parent = slicer.qMRMLWidget()
       self.parent.setLayout(qt.QVBoxLayout())
@@ -30,8 +32,11 @@ class moduloTarea1Widget:
 
   def setup(self):
 
+
     #Path para camilo= path='C:\Users\Camilo_Q\Documents\MEGA\Trabajo_de_grado\moduloTarea1/Tornillo_1.STL'
     #Path para Nicolas = ...
+
+#---------------------------------------------------------------------------------------------------
 
     sampleCollapsibleButton = ctk.ctkCollapsibleButton() #Se crea boton colapsable
     sampleCollapsibleButton.text = "Creacion de tornillos" #Se asigna label del boton colapsable
@@ -49,20 +54,48 @@ class moduloTarea1Widget:
     #Click boton "Cargar tornillo"
     sampleFormLayout = qt.QFormLayout(sampleCollapsibleButton)
 
-    layout1Button = qt.QPushButton("Cargar tornillo") #Se crea boton pulsable, con texto "Apply"
+    layout1Button = qt.QPushButton("Cargar tornillo 1") #Se crea boton pulsable, con texto "Apply"
     layout1Button.toolTip = "Al presionar aparecerá un tornillo en la scena" #Información que aparece si dejas el mouse encima
     sampleFormLayout.addWidget(layout1Button) #Se añade el boton al layout del boton colapsable
     layout1Button.connect('clicked(bool)',self.onApply)
 
+    layout2Button = qt.QPushButton("Cargar tornillo 2") #Se crea boton pulsable, con texto "Apply"
+    layout2Button.toolTip = "Al presionar aparecerá un tornillo en la scena" #Información que aparece si dejas el mouse encima
+    sampleFormLayout.addWidget(layout2Button) #Se añade el boton al layout del boton colapsable
+    layout2Button.connect('clicked(bool)',self.onApply2)
+
 #-----------------------------------------------------------------------------------------------
 
-    path='C:\Users\Camilo_Q\Documents\MEGA\Trabajo_de_grado\moduloTarea1/Tornillo_1.STL' #Se obtiene direccion de la unbicación del tornillo
-    slicer.util.loadModel(path) #Se carga el tornillo al espacio 3d
-    tornillo1=slicer.util.getNode('Tornillo_1') #Se obtiene la informacion del tonillo cargado
-    transformada=slicer.vtkMRMLLinearTransformNode() #Se crea una transformada lineal
-    transformada.SetName('Transformada') #Se asigna nombre a la transformada
-    slicer.mrmlScene.AddNode(transformada) #
-    tornillo1.SetAndObserveTransformNodeID(transformada.GetID()) # Se relaciona la trnasformada con el objeto tornillo
-
   def onApply(self): 
-    print "Cargaste un tornillo"
+
+      print "Cargaste tornillo 1"
+
+      path='C:\Users\Camilo_Q\Documents\MEGA\Trabajo_de_grado\moduloTarea1/Tornillo_1.STL' #Se obtiene direccion de la unbicación del tornillo
+      slicer.util.loadModel(path) #Se carga el tornillo al espacio 3d
+      tornillo1=slicer.util.getNode('Tornillo_1') #Se obtiene la informacion del tonillo cargado
+      transformada=slicer.vtkMRMLLinearTransformNode() #Se crea una transformada lineal
+      transformada.SetName('Transformada Tornillo 1') #Se asigna nombre a la transformada
+      slicer.mrmlScene.AddNode(transformada) #
+      tornillo1.SetAndObserveTransformNodeID(transformada.GetID()) # Se relaciona la trnasformada con el objeto tornillo
+      numeroTornillo=1
+
+  def onApply2(self): 
+
+      print "Cargaste tornillo 2"
+
+      path='C:\Users\Camilo_Q\Documents\MEGA\Trabajo_de_grado\moduloTarea1/Tornillo_1.STL' #Se obtiene direccion de la unbicación del tornillo
+      slicer.util.loadModel(path) #Se carga el tornillo al espacio 3d
+      tornillo2=slicer.util.getNode('Tornillo_1') #Se obtiene la informacion del tonillo cargado
+      transformada2=slicer.vtkMRMLLinearTransformNode() #Se crea una transformada lineal
+      transformada2.SetName('Transformada Tornillo 2') #Se asigna nombre a la transformada
+      slicer.mrmlScene.AddNode(transformada2) #
+      tornillo2.SetAndObserveTransformNodeID(transformada2.GetID()) # Se relaciona la trnasformada con el objeto tornillo
+      
+      matriztornillo2 = vtk.vtkMatrix4x4()
+      transformada2.GetMatrixTransformToParent(matriztornillo2)
+      matriztornillo2.SetElement(0,3,5)
+      transformada2.SetAndObserveMatrixTransformToParent(matriztornillo2)
+
+   
+
+  
