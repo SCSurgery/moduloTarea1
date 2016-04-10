5544424f5a3887ffa93910d53fe5ade5310816a0
qcamiloq <qcamiloq@gmail.com>
2016-04-09 22:53:09 -0500

    inicio

---
+++ moduloTarea1.py
@@ -0,0 +1,32 @@
+# -*- coding: UTF-8 -*-
+from __main__ import vtk, qt, ctk, slicer
+
+class moduloTarea1():
+  def __init__(self, parent):
+    parent.title = "Tarea 1"
+    parent.categories = ["Tareas"]
+    parent.dependencies = []
+    parent.contributors = ["Camilo Quiceno Q"] # replace with "Firstname Lastname (Org)"
+    parent.helpText = """
+    Este modulo sirve para cambiar el layout del slicer
+    """
+    parent.acknowledgementText = """
+    Desarrollado por Camilo Quiceno 
+    """ # replace with organization, grant and thanks.
+    self.parent = parent
+
+class moduloTarea1Widget:
+  def __init__(self, parent = None):
+    if not parent:
+      self.parent = slicer.qMRMLWidget()
+      self.parent.setLayout(qt.QVBoxLayout())
+      self.parent.setMRMLScene(slicer.mrmlScene)
+    else:
+      self.parent = parent
+    self.layout = self.parent.layout()
+    if not parent:
+      self.setup()
+      self.parent.show()
+
+  def setup(self):
+    print "Estoy en el moduloTarea1"
